2b4e2d504ce95af2f4f803168a0f7b3f7feeb127
vaishnavi-0001 <140255751+vaishnavi-0001@users.noreply.github.com>
2025-05-14 20:08:17 +0530

    Add middlewware and set coverage

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,13 @@ module.exports = {
       testEnvironment: "node",
       moduleFileExtensions: ["ts", "js"],
       verbose: true,
+      collectCoverage: true,
+      coverageProvider: "v8",
+      collectCoverageFrom:[
+            "src/**/*.ts",
+            "!test/**",
+            "!**/node_modules/**",
+      ],
       transform: {
             "^.+\.tsx?$": ["ts-jest", {}],
       },
--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -1,55 +1,57 @@
-import express, { NextFunction, Response } from "express";
-import { TenantController } from "../controllers/TenantController";
-import { TenantService } from "../services/TenantService";
-import { AppDataSource } from "../config/data-source";
-import { Tenant } from "../entity/Tenant";
-import logger from "../config/logger";
-import authenticate from "../middlewares/authenticate";
-import { canAccess } from "../middlewares/canAccess";
-import { Roles } from "../constants";
-import tenantValidator from "../validator/tenant-validator";
-import { CreateTenantRequest } from "../types";
+import express, { NextFunction, Response } from "express"
+import { TenantController } from "../controllers/TenantController"
+import { TenantService } from "../services/TenantService"
+import { AppDataSource } from "../config/data-source"
+import { Tenant } from "../entity/Tenant"
+import logger from "../config/logger"
+import authenticate from "../middlewares/authenticate"
+import { canAccess } from "../middlewares/canAccess"
+import { Roles } from "../constants"
+import tenantValidator from "../validator/tenant-validator"
+import { CreateTenantRequest } from "../types"
 
-const router = express.Router();
+const router = express.Router()
 
-const tenantRepository = AppDataSource.getRepository(Tenant);
-const tenantService = new TenantService(tenantRepository);
-const tenantController = new TenantController(tenantService, logger);
+const tenantRepository = AppDataSource.getRepository(Tenant)
+const tenantService = new TenantService(tenantRepository)
+const tenantController = new TenantController(tenantService, logger)
 
 router.post(
-    "/",
-    authenticate,
-    canAccess([Roles.ADMIN]),
-    tenantValidator,
-    async (req: CreateTenantRequest, res: Response, next: NextFunction) => {
-        try {
-            await tenantController.create(req, res, next);
-        } catch (error) {
-            next(error);
-        }
-    },
-);
+      "/",
+      authenticate,
+      canAccess([Roles.ADMIN]),
+      tenantValidator,
+      async (req: CreateTenantRequest, res: Response, next: NextFunction) => {
+            try {
+                  await tenantController.create(req, res, next)
+            } catch (error) {
+                  next(error)
+            }
+      },
+)
 
 router.patch(
-    "/:id",
-    authenticate,
-    canAccess([Roles.ADMIN]),
-    tenantValidator,
-    async (req: CreateTenantRequest, res: Response, next: NextFunction) => {
-        try {
-            await tenantController.update(req, res, next);
-        } catch (error) {
-            next(error);
-        }
-    },
-);
-router.get("/", (req, res, next) => tenantController.getAll(req, res, next));
-router.get("/:id", (req, res, next) => tenantController.getOne(req, res, next));
+      "/:id",
+      authenticate,
+      canAccess([Roles.ADMIN]),
+      tenantValidator,
+      async (req: CreateTenantRequest, res: Response, next: NextFunction) => {
+            try {
+                  await tenantController.update(req, res, next)
+            } catch (error) {
+                  next(error)
+            }
+      },
+)
+router.get("/", (req, res, next) => tenantController.getAll(req, res, next))
+router.get("/:id", authenticate, canAccess([Roles.ADMIN]), (req, res, next) =>
+      tenantController.getOne(req, res, next),
+)
 router.delete(
-    "/:id",
-    authenticate,
-    canAccess([Roles.ADMIN]),
-    (req, res, next) => tenantController.destroy(req, res, next),
-);
+      "/:id",
+      authenticate,
+      canAccess([Roles.ADMIN]),
+      (req, res, next) => tenantController.destroy(req, res, next),
+)
 
-export default router;
+export default router
